37ca263c2958ac6699ef3539560a5da3508d9f8c
aylin <a.sanlazo@gmail.com>
2022-10-21 11:06:00 -0300

    test

---
+++ migrations/05-seed_data.sql
@@ -0,0 +1,6 @@
+\c ecommerce 
+
+\COPY customers FROM 'C:/Users/user/intro_dev/sql-test-prep-aylinsanmartin/data/customers.csv'csv HEADER;
+\COPY products FROM 'C:/Users/user/intro_dev/sql-test-prep-aylinsanmartin/data/products.csv'csv HEADER;
+\COPY details FROM 'C:/Users/user/intro_dev/sql-test-prep-aylinsanmartin/data/purchase_details.csv'csv HEADER;
+\COPY purchases FROM 'C:/Users/user/intro_dev/sql-test-prep-aylinsanmartin/data/purchases.csv'csv HEADER;
